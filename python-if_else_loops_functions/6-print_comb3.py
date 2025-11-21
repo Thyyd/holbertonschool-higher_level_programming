d7396ac3e00875d5556688cff0b7e6c3c7146730
Thyyd <roastingblade@mgail.com>
2025-11-21 15:56:37 +0000

    Ajout du programme loop avec condition : print_comb3

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(8):
-	for j in range(10):
-		if(10*i+j)%10 > i:
-			print("{:02}".format(10*i+j), end=", ")
+    for j in range(10):
+        if (10 * i + j) % 10 > i:
+            print("{:02}".format(10*i+j), end = ", ")
 
 print("89")
