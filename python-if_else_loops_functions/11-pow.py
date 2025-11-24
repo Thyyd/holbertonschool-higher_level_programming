9d9299c47a2ae2f8f5cb91a305655fb5b470d6c5
Thyyd <rostingblade@gmail.com>
2025-11-24 10:31:48 +0000

    Ajout du fichier add

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def pow(a, b):
     res = 1
-    for i in range(b):
-        res *= a
+    for i in range(abs(b)):
+        if b < 0:
+            res /= a
+        else:
+            res *= a
 
     return res
