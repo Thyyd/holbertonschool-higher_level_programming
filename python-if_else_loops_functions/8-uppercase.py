f8e07533b936d14db31b4f753fe7d18071282bfa
Thyyd <rostingblade@gmail.com>
2025-11-24 10:10:38 +0000

    Ajout du fichier uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,9 +2,10 @@
 def uppercase(str):
     renv = ""
     for car in range(len(str)):
-	if ord('a') <= ord(str[car]) <= ord('z'):
-	    att = ord(str[car]) - 32
-	    renv += chr(att)
-	else:
-	    renv += str[car]
-    print(renv)
+        if ord('a') <= ord(str[car]) <= ord('z'):
+            att = ord(str[car]) - 32
+            renv += chr(att)
+        else:
+            renv += str[car]
+
+    print("{}".format(renv))
