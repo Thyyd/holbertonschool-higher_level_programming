2d1e789e39273b63bc72fb04d10ae659e9ea5ea2
Thyyd <roastingblade@gmail.com>
2025-11-24 14:43:10 +0000

    Ajout square v.0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
-"""
-Module contenant la classe Square
-"""
+""" Module contenant la classe Square """
 
 class Square:
-    """
-    Classe qui definit un carre. 
-    Est actuellement vide, car il s'agit de la V.0
-    """
+    """Classe qui definit un carre. 
+    Est actuellement vide, car il s'agit de la V.0"""
     pass
