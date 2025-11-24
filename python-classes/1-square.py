85d735538d30129239475cb8fdb11f8297db9a1c
Thyyd <roastingblade@gmail.com>
2025-11-24 15:15:57 +0000

    Ajout square v.0.1

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" Module contenant la classe Square """
+
+
+class Square:
+    """Classe qui definit un carre. Definition d'un attribut prive size (Ajout V.0.1)"""
+    def __init__(self, size):
+        self.__size = size
