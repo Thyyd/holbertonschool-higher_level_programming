b85b17c417afa78e035335b72dfa81c050ad3453
Thyyd <roastingblade@gmail.com>
2025-11-25 16:23:25 +0000

    Ajout square v.0.6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -78,6 +78,7 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print("")
+            return
         for i in range(self.__position[1]):
                 print("")
         for j in range(self.__size):
