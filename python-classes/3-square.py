9f8b4e8ccf745067a216f84cb75b4cf5c44fedeb
Thyyd <roastingblade@gmail.com>
2025-11-25 08:35:15 +0000

    Ajout square v.0.2

---
+++ python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+""" Module contenant la classe Square """
+
+
+class Square:
+    """Classe qui definit un carre. Definition d'un attribut prive
+    size et verifie que size soit un entier positif (Ajout V.0.2).
+    Ajout d'une methode renvoyant l'aire du carre (Ajout v.0.3)"""
+    def __init__(self, size=0):
+        try:
+            if type(size) is not int:
+                raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+
+        except (TypeError, ValueError) as e:
+            raise e
+
+        self.__size = size
+
+    def area(self):
+        print(self.__size*self.__size)
