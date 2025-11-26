a35b14043aa191c0b3ed2659158129740e051425
Thyyd <roastingblade@gmail.com>
2025-11-26 10:15:37 +0000

    Ajout fichiers add_integer.py et sont fichier test dans le repertoire test

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module contenant une fonction ajoutant deux nombres.
+"""
+
+def add_integer(a, b=98):
+    """ 
+    Fonction add_integer qui ajoute les deux nombres entres en parametres.
+
+    Parametres :
+	a : premier terme a ajouter (int ou float)
+        b : deuxieme terme a ajouter (int ou float, valeur par defaut 98)
+
+    Return :
+       	Somme entiere de a et b
+	
+    Raise :
+        TypeError si a ou b n'est pas un int ou un float
+    """
+
+    Try:
+        if not isinstance(a, (int, float)):
+            raise TypeError("a must be an integer")
+        if not isinstance(b, (int, float)):
+            raise TypeError("b must be an integer")
+
+    except (TypeError) as e:
+        raise e
+
+    return int(a) + int(b)
