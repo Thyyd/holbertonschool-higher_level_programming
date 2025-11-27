ca3fbcab3da325b7850e3aa877688d51a1c42d42
Thyyd <roastingblade@gmail.com>
2025-11-27 15:23:43 +0100

    Ajout fichier text_indentation.py et son fichier test.txt

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module contenant une fonction affichant un texte aere
+"""
+
+
+def text_indentation(text):
+    """
+    Fonction text_indentation qui retourne a la ligne à chaque '.', '?' et ':'
+
+    Parametres :
+        - text : string
+
+    Return :
+        Un texte qui est separe a chaque signe de ponctuation par un saut de
+        lignes.
+
+    Raise :
+        - TypeError si text n'est pas un string
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    a_afficher = ""
+    for i in text:
+        a_afficher += i
+        if i == "." or i == "?" or i == ":":
+            print(a_afficher)
+            print("")
+            a_afficher = ""
+    print(a_afficher)
+
+
+# text_indentation("Bonjour. Arrive-t-on ? bientot")
